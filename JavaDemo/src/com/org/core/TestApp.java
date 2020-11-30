1e86b57e51332f176eecb044e4df2465009b500e
dariya vali <dariya vali@192.168.0.9>
2020-11-30 12:34:04 +0530

    to local

--- JavaDemo/src/com/org/core/TestApp.java
+++ JavaDemo/src/com/org/core/TestApp.java
@@ -2,6 +2,6 @@ package com.org.core;
 
 public class TestApp {
 	public static void main(String[] args) {
-		System.out.println("welcome to git repository...!!!!!");
+		System.out.println("welcome to git repository...!!!!! "); 
 	}
 }
