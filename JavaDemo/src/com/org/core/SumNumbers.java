e7a9094b3f7e026ccc05048f2f1a64afd079b674
dariya vali <dariya vali@192.168.0.9>
2020-11-30 13:08:47 +0530

    by dariya

--- JavaDemo/src/com/org/core/SumNumbers.java
+++ JavaDemo/src/com/org/core/SumNumbers.java
@@ -2,4 +2,9 @@ package com.org.core;
 
 public class SumNumbers {
 
+	public int getSum(int a, int b) {
+
+		return a + b;
+	}
+	
 }
--- JavaDemo/src/com/org/core/TestApp.java
+++ JavaDemo/src/com/org/core/TestApp.java
@@ -4,5 +4,8 @@ public class TestApp {
 	public static void main(String[] args) {
 		
 		System.out.println("hai");
+		int sum = new SumNumbers().getSum(10, 20);
+		System.out.println(sum);
+		
 	}
 }
